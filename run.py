1852bee2407f1ffe252375dc5cb53d7658d37c34
yann6182 <yannickngaleu6@gmail.com>
2025-03-25 14:55:13 +0100

    requiement.txt

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # 8000 par défaut en local
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
