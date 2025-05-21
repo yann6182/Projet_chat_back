937d0502cec1b114dae514f2a86cffa81280c232
yann6182 <114214473+yann6182@users.noreply.github.com>
2025-05-21 18:47:36 +0200

    Update init_db.py

--- app/init_db.py
+++ app/init_db.py
@@ -1,7 +1,6 @@
 from db.database import engine, Base
 
-# ✅ Cette ligne importe tous les modèles pour qu’ils soient enregistrés
 
-print("📦 Initialisation de la base de données...")
+print("Initialisation de la base de données...")
 Base.metadata.create_all(bind=engine)
-print("✅ Tables créées avec succès.")
+print(" Tables créées avec succès.")
